a8fde7935a18ba815fda2be4ba99c11bcff675f2
eyaelja@gmail.com <eyaelja1@gmail.com>
2019-02-11 14:02:54 -0500

    hgsfhjfhs hfhf

--- src/newassign.java
+++ src/newassign.java
@@ -4,17 +4,18 @@
    public class newassign {
        public static void main(String args[]) {
            Random rand = new Random();
-           int secretNum = rand.nextInt(10) + 1;
+           int secretNum = 1+rand.nextInt(10);
            int guess;
            String Guess;
 
            Scanner keyboard = new Scanner(System.in);
-           System.out.println("I have chosen a number between 1 and 10. Try to guess it");
+          do {System.out.println("I have chosen a number between 1 and 10. Try to guess it");
            guess = keyboard.nextInt();
-           do {
 
-               if (guess == secretNum) {
-                   System.out.println(" your guess");
+
+               System.out.println(" your guess");
+                   if (guess == secretNum) {
+                       System.out.println("Thats right! you're a good guesser");
                } else {
                    System.out.println("That is incorrect. Guess again.");
 
