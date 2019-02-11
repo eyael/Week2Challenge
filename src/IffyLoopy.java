967086337e4a02263376c3716f3bd6262135a8b3
eyaelja@gmail.com <eyaelja1@gmail.com>
2019-02-11 11:53:02 -0500

    first commit

--- src/IffyLoopy.java
+++ src/IffyLoopy.java
@@ -4,30 +4,40 @@ import java.util.Scanner;
 public class IffyLoopy {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-      //  Random input = new Random();
+        Random input = new Random();
         String response = "";
         boolean redEyes;
 
         //String[] Loopy = {"Red", "Yellow", "Blue", "Orange", "Pink"};
-        for (int i = 1; i <= 5; i++) {
-            int Loppyv = input.nextInt(5);
+        // for (int i = 1; i <= 5; i++) {
+        do {
+            System.out.println("Are your Eyes red?");
+            response = sc.nextLine();
+            if (response.equalsIgnoreCase("y")||(response.equalsIgnoreCase("yes"))) {
+                System.out.println("Get some sleep!");
+            }else
+                System.out.println("You look great");
 
-
-            do {
-                System.out.println("Are your Eyes red?");
-                response = sc.nextLine();
-                if (response.equalsIgnoreCase("y")) {
-                    System.out.println("Get some sleep!");
-                }else
-                    System.out.println("You look great");
-
-                System.out.println("Do you want to try again?");
-                redEyes =sc.nextBoolean();
-
-
-
-            }while(response.equalsIgnoreCase("n"));
-            System.out.println("n");
-        }
+            System.out.println("Do you want to try again? ..press (any key to continue)..press(NO|N) to exit");
+            response = sc.nextLine();
+            if(response.equalsIgnoreCase("NO")||(response.equalsIgnoreCase("N"))){
+                System.out.println("Thanks you gonna exit");
+                System.exit(0);
+            }
+        }while(true);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/newassign.java
@@ -0,0 +1,17 @@
+
+    //import java.util.Random;
+   // import java.util.Scanner;
+  //  public class newassign {
+        //public static void main(String args[]){
+           /* Random rand = new Random();
+            int secretNum= rand.nextInt(10)+1;
+            int guess;
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("I have chosen a number between 1 and 10. Try to guess it");
+            do {{
+
+
+        }
+
+
+}*/
